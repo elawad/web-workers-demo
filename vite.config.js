2c634cc76fa6cafd67830976fd3806473fbada4a
Aymen Elawad <elawad@users.noreply.github.com>
2023-02-16 12:50:59 -0500

    vite base

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  base: isProd ? '/web-workers-demo/' : '/',
   server: { open: true, host: true },
 });
