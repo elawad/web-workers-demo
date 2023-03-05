e6971b9e4baf0f2ae2e83f0f588b19ce407f5310
Aymen Elawad <elawad@users.noreply.github.com>
2023-03-05 15:25:56 -0500

    lazy load slides

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react';
+import { StrictMode, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import Slides from './Slides';
+const Slides = lazy(() => import('./Slides'));
 
 const app = window.location.search === '?app';
 
